5bb6561963899523aeaecdf1554711def18f6310
roller0516 <roller0516@naver.com>
2023-10-26 01:52:35 +0900

    include path

--- aos/Source/aos/aos.Build.cs
+++ aos/Source/aos/aos.Build.cs
@@ -9,5 +9,7 @@ public class aos : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+
+		PrivateIncludePaths.Add("aos");
 	}
 }
